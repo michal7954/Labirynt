3f785175c42d3fac9a66f549a89d2dd3d57f9af2
michal7954 <michal7954@onet.pl>
2018-03-05 19:21:45 +0100

    Event click, przygotowanie danych
    2018-03-05
    Duże zmiany, pierwszy etap pracy w domu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+    var data = [];
+    var hexy = [];
+
     //----------- PRZYGOTOWANIE PANELU KONTROLNEGO
 
     var size = $("<select>").attr("id", "size");
@@ -12,6 +15,7 @@ $(document).ready(function () {
     $("#control").append(size);
 
     var buttons = ["wall", "enemy", "treasure", "light"];
+    var choosen = "wall";
 
     for (i = 0; i < buttons.length; i++) {
         var button = $("<button>")
@@ -20,7 +24,8 @@ $(document).ready(function () {
             .click(function () {
                 $("button").attr("class", "");
                 $(this).attr("class", "choosen");
-                console.log($(this).attr("id"))
+                choosen = $(this).attr("id");
+                console.log(choosen)
             })
         $("#control").append(button);
     }
@@ -29,7 +34,7 @@ $(document).ready(function () {
     //---------- EVENTS
 
     $("#root").empty();
-    draw(3);
+    draw(5);
 
     $("#size").on("change", function () {
         $("#root").empty();
@@ -38,12 +43,15 @@ $(document).ready(function () {
     });
 
     function draw(size) {
+        hexy = []
         for (i = 0; i < size; i++) {        //kolumny
             for (j = 0; j < size - 1; j++) {    //wiersze
                 var pole = $("<div>")
                     .attr("class", "pole")
                     .css("left", i * 87)
                     .css("top", j * 100)
+                    .data("x", i)
+                    .data("y", j)
 
                 if (i % 2 == 0) {
                     pole.css("top", j * 100)
@@ -51,15 +59,60 @@ $(document).ready(function () {
                 else {
                     pole.css("top", j * 100 + 50)
                 }
+                hexy.push(pole[0])
                 $("#root").append(pole);
             }
         }
+
+        console.log(hexy)
+
+        $(".pole").on("click", function (e) {
+            //console.log(e)
+            //console.log(hexy.indexOf(e.currentTarget))
+
+            //$(e.currentTarget).empty();
+
+
+            var obj = { index: hexy.indexOf(e.currentTarget), number: data.length, x: $(e.currentTarget).data("x"), y: $(e.currentTarget).data("y"), element: e.currentTarget, poz: 0, dirOut: 0, dirIn: 3, type: choosen[0].toUpperCase(), long_type: choosen }
+
+            var num = -1;
+            var istnieje = false;
+
+            for (i = 0; i < data.length; i++) {
+                if (data[i].index == hexy.indexOf(e.currentTarget)) {
+                    istnieje = true
+                    num = i;
+                }
+            }
+            if (!istnieje) {
+                var arr = $("<div>")
+                    .attr("class", "arr")
+                $(e.currentTarget).append(arr);
+
+                var p = $("<p>")
+                    .text(obj.type)
+                obj.element.append(p[0])
+
+                data.push(obj)
+
+            }
+            else {
+                data[num].poz++;
+                data[num].dirOut = (data[num].dirOut + 1) % 6;
+                data[num].dirIn = (data[num].dirIn + 1) % 6;
+                data[num].element.children[0].style.transform = "rotate(" + data[num].poz * 60 + "deg)";
+
+                data[num].type = choosen[0].toUpperCase()
+                data[num].element.children[1].innerHTML = data[num].type
+            }
+
+
+            console.log(data)
+
+        })
     }
 
-    /*
-    $(".button").on("click", "div", function (e) {
-        
-        console.log($(".choosen")[0].attr("id"))
-    })
-    */
+    //----------- EVENT KLIK HEXAGON
+
+
 })
